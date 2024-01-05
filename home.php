80490c774c71cf1f72898be3b572d14ac2454b42
Krupal <krupal561@gmail.com>
2024-01-05 19:32:12 +0530

    enc second commit

--- home.php
+++ home.php
@@ -13,17 +13,44 @@ if (!isset($_SESSION['username'])) {
     <title>Document</title>
 
     <style>
-             body{
-                background-image:url("img/bg.jpg");
-                background-repeat:no-repeat;
-                background-size: 99.99% 99.99%;
-                background-attachment: fixed;
-                margin: 0;
+          
+		body{
+            background-image:url("img/bg.jpg");
+            background-repeat:no-repeat;
+            background-size: 99.99% 99.99%;
+            background-attachment: fixed;
+            margin: 0;
             font-family: Arial, sans-serif;
             }
+        header {
+            background-color: #333;
+            padding: 15px;
+            display: flex;
+            justify-content: space-between;
+            align-items: center;
+            color: white;
+        }
+
+        .logo {
+            width: 27%; /* Adjust the width as needed */
+            height: auto;
+        }
+
+        nav {
+            display: flex;
+        }
+
+        nav a {
+            color: white;
+            text-decoration: none;
+            margin-left: 25px;
+        }
+		
+		
+            
 
             h2{
-                    font-size:29px;
+                    font-size:26px;
                     color:white;
                     text-shadow:0px 0px 5px #1ebfdd,0px 0px 5px #1ebfdd;
                     transition:all ease 0.5s;
@@ -60,8 +87,6 @@ if (!isset($_SESSION['username'])) {
                     }
                  }
                  
-            
-                 
     </style>
 </head>
 
@@ -75,16 +100,22 @@ if (!isset($_SESSION['username'])) {
 
 <body>
     <form action="" method="POST">
-        <div class="Header">
-            <div class="logo-container">
-                <table>
+       
+    <header>
+    <div class="logo">
+       <table>
                     <tr>
                     <td><h2> Shopify's</h2></td><td> <img src="img/logo.png"  alt="Rotating Image" class="rotating-logo"></td> <td><h2>Shopping</h2></td>
                     </tr>
                 </table>
-            </div>
-        </div>
-        
+    </div>
+    <nav>
+        <a href="#">Home</a>
+        <a href="#">About</a>
+        <a href="#">Services</a>
+        <a href="#">Contact</a>
+    </nav>
+</header>
     <input type="submit" name="btnlogout" value="Logout">
 </form>
 </body>
