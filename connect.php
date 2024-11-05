191221342f717a1b836be70135a87c81cf2b08d1
Krupal Patel <patelkrupal679@gmail.com>
2024-11-05 18:35:13 +0530

    For Redeme File

--- connect.php
+++ connect.php
@@ -3,3 +3,4 @@ error_reporting(E_ALL & ~E_WARNING);
 $connect = mysqli_connect("localhost", "root", "", "bid_bazzar");
 if (!$connect)
     echo "Can not connect to database" or die(mysqli_connect_error());
+?>
